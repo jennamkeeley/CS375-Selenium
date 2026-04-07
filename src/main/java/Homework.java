54f3a77469255677fb4f56ff4eacc9cb75358cdd
Jenna Keeley <jennakeeley@Jennas-MacBook-Air.local>
2026-04-07 14:00:06 -0500

    Keeley QS#09 HW Solution pushed to forked repository

--- src/main/java/Homework.java
+++ src/main/java/Homework.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
 public class Homework {
     public static void RunHomework() {
         try (Scanner scanner = new Scanner(System.in)) {
@@ -12,11 +20,35 @@ public class Homework {
     // HOMEWORK: Use Selenium and/or Jsoup to pull the url and title of the main youtube video on the page
     // HINT: You will need to click the button for the video to open so you can access the src attribute on the iframe that has the url
     // Both of these are implemented in a similar fashion in the methods defined above so use those as a guide
-    static String DisplayMainYoutubeVideoDetails() {
+   static String DisplayMainYoutubeVideoDetails() {
         String url = "";
         String title = "";
 
-        // Your code goes here
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            driver.get("https://acusports.com/");
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+            WebElement youtubeSection = wait.until(
+                ExpectedConditions.presenceOfElementLocated(By.id("main-youtube-grid"))
+            );
+
+            WebElement titleElement = youtubeSection.findElement(
+                By.cssSelector("h3[id^='main-youtube-grid-item-title']")
+            );
+            title = titleElement.getText();
+
+            WebElement linkElement = youtubeSection.findElement(
+                By.xpath("//a[contains(@href, 'youtube.com/watch')]")
+            );
+            url = linkElement.getAttribute("href");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
 
         return "Title: " + title + " Url: " + url;
     }
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,6 +1,6 @@
 public class Program {
-    static boolean runDemo = true;      // When running the material from the demo make sure this is true
-    static boolean runHomework = false; // When testing your homework make sure this is true
+    static boolean runDemo = false;      // When running the material from the demo make sure this is true
+    static boolean runHomework = true; // When testing your homework make sure this is true
 
     public static void main(String[] args) {
         if (runDemo) {
